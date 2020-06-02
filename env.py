4ed10561145b84fa3e26592beba357214027bb9c
Jussi Nousiainen <juno.athome@gmail.com>
2020-06-02 12:57:38 +0200

    Ststic and media files for the meeting with Brian

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_8BxDes6aImuFsSX1zG67350Y00WIbWGPF1")
+os.environ.setdefault("STRIPE_SECRET", "sk_test_EUw0UmpcDIHrE62hnvo2FA7o00fuQddBJ1")
