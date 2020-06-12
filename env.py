f60698809703cdd06801cfbc16dd83a67f0d78eb
Jussi Nousiainen <juno.athome@gmail.com>
2020-06-12 15:51:46 +0200

    remove env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_8BxDes6aImuFsSX1zG67350Y00WIbWGPF1")
-os.environ.setdefault("STRIPE_SECRET", "sk_test_EUw0UmpcDIHrE62hnvo2FA7o00fuQddBJ1")
-
-os.environ.setdefault("EMAIL_ADDRESS", "juno.athome@gmail.com")
-os.environ.setdefault("EMAIL_PASSWORD", "1kaunisaamu")
