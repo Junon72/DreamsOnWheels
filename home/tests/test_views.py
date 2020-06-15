d15afb4618f9b97f28ed3736554cafeb378181ee
Jussi Nousiainen <juno.athome@gmail.com>
2020-06-15 17:58:02 +0200

    recommit after git crashe

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -5,26 +5,27 @@ from home.views import *
 from products.models import Product, Original
 
 
-# class TestGetListOfProductsOr404(TestCase):
-#     def test_get_list_or_404(self):
-#         p1 = Product.objects.create(make="Ford")
-#         p2 = Product.objects.create(make="Ford")
+class TestGetListOfProductsOr404(TestCase):
+    def test_get_list_or_404(self):
+        p1 = Product.objects.create(make="Ford")
+        p2 = Product.objects.create(make="Mustang")
         
-#         with self.assertRaises(Http404):
-#             get_list_or_404(Product.objects.none(), make__contains="Ford")
+        with self.assertRaises(Http404):
+            get_list_or_404(Product.objects.all(), make__contains="Ford")
+            get_list_or_404(Product.objects.all(), make__contains="Monkey")
 
-class TestIndexViews(TestCase):
+# class TestIndexViews(TestCase):
 
-    def setUp(self):
-        self.client = Client()
-        self.index_url = reverse('index')
-        self.product_url = reverse('products:all_products', args=[1])
-        self.product1 = Product.objects.create(pk=1)
-        self.original_url = reverse('products:get_original', args=[1])
-        self.original1 = Original.objects.create(pk=1)
+#     def setUp(self):
+#         self.client = Client()
+#         self.index_url = reverse('index')
+#         self.product_url = reverse('products:all_products', args=[1])
+#         self.product1 = Product.objects.create(pk=1)
+#         self.original_url = reverse('products:get_original', args=[1])
+#         self.original1 = Original.objects.create(pk=1)
 
-    def test_get_index_page(self):
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+#     def test_get_index_page(self):
+#         response = self.client.get(reverse('index'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'index.html')
 
